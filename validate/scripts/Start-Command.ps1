648d27a9545036a5d28f4a6552944e8953adb02d
Callum Shackleton <silentlisten@googlemail.com>
2016-04-21 22:45:01 +0100

    added new command script

---
+++ validate/scripts/Start-Command.ps1
@@ -0,0 +1,28 @@
+﻿# Simple script for running strings as commands and catching and returning errors if any are found.
+# Useful when running native Windows applications such as .exe's
+
+Param (
+
+    [string]$command
+
+)
+
+$ErrorActionPreference = 'Stop'
+
+try {
+
+    Invoke-Expression -Command $command
+
+    if ($LASTEXITCODE -gt 0) { throw }
+
+    else { exit $LASTEXITCODE }
+
+}
+
+catch {
+
+    Write-Output "*** Error Encounterd ***`nLast exit code: $LASTEXITCODE `nCommand: $command `nError message: $_"
+
+    exit $LASTEXITCODE
+
+}
