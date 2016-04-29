d000395bb92e6d0ff17be1f9063c40425b9f9658
Callum Shackleton (Admin) <cshackleton.admin@zeninternet.co.uk>
2016-04-29 17:51:53 +0100

    packer update

--- validate/packer/packer-validate.ps1
+++ validate/packer/packer-validate.ps1
@@ -1,4 +1,17 @@
-﻿$command = 'packer validate .\build\packer\packer-template.json'
+﻿try {
+    
+    Import-Module -Name aws*
+    Set-DefaultAWSRegion -Region $env:AWS_region
+    
+    $latestami = (Get-EC2ImageByName -Name $imagename).ImageId
+    
+    $env:PK_VAR_source_ami = $latestami
+    
+}
+catch {}
+
+
+$command = 'packer validate .\build\packer\packer-template.json'
 
 .\resources\scripts\start-command.ps1 -command $command
 
