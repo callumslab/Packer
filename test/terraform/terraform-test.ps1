8a69bc2b3f99124bd36f99c2370052f2867b2777
Callum Shackleton (Admin) <cshackleton.admin@zeninternet.co.uk>
2016-05-03 16:43:17 +0100

    terraform fix

--- test/terraform/terraform-test.ps1
+++ test/terraform/terraform-test.ps1
@@ -2,22 +2,21 @@ $ErrorActionPreference = 'Stop'
 
 try {
     
-    Import-Module -Name aws*
+    $thisinstance = .\resources\scripts\get-instancedata.ps1
     
-    Set-DefaultAWSRegion -Region $env:AWS_region
+    $latestami = .\resources\scripts\get-latestami.ps1 -imagename $env:AWS_Image_Name
     
-    $thisinstanceid = (Invoke-WebRequest -Uri 'http://169.254.169.254/latest/meta-data/instance-id').content
-    
-    $thisinstance = (Get-EC2Instance -Instance $thisinstanceid).runninginstance
     
     $tempkeypair = New-EC2KeyPair -KeyName "terraform-$(Get-Random -Minimum 001 -Maximum 999)"
     
-    # A temp sec group should be created on the fly
+    
+    # A temp sec group should be created on the fly, should match Packer
     $secgroupname = 'IaC-Testing-ZenMgmtSecGroup'
     
     $secgroupid = (Get-EC2SecurityGroup | where groupname -Match $secgroupname).groupid
     
     
+    # This section should be replaced by a property that is retreived and matched from the AMI 
     $inputpath = '.\build\output'
 
     $AMIfile = Get-ChildItem $inputpath | where name -like *ami*                               
@@ -25,6 +24,7 @@ try {
     [string]$AMIvalue =  $AMIfile | Get-Content
     
     $env:TF_VAR_packer_ami = $AMIvalue
+    # End section - getting AMI
     
     
     $outputpath = '.\test\output'
@@ -34,7 +34,6 @@ try {
     }
     
     
-
     # This could be fed in from GoCD as an environment variable
     $rolename = 'IaC-Testing-GoCDBuildServer'
 
@@ -43,18 +42,21 @@ try {
     if (-not ($rolecreds)) { throw }
 
 
+    # Setting env variables needed by terraform
     $env:TF_VAR_access_key = $rolecreds.accesskeyid
     $env:TF_VAR_secret_key = $rolecreds.secretaccesskey
     $env:TF_VAR_token = $rolecreds.token
-    $env:TF_VAR_region = $env:AWS_region
-    $env:TF_VAR_keypair = $tempkeypair.keyname
-    $env:TF_VAR_subnet_id = $thisinstance.subnetid
+    $env:TF_VAR_region = $thisinstance.region
     $env:TF_VAR_vpc_security_group_ids = $secgroupid
+    $env:TF_VAR_subnet_id = $thisinstance.subnetid
+    $env:TF_VAR_keypair = $tempkeypair.keyname
+    
 
 
     $command = "terraform apply -state='$outputpath\terraform.tfstate' -backup=- .\test\terraform"
 
     .\resources\scripts\start-command.ps1 -command $command
+
 
     # Insert terraform destroy command here
 
