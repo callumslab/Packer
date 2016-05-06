7964edbe6bf63c8413dc8b105a9bbc3378c46782
Callum Shackleton (Admin) <cshackleton.admin@zeninternet.co.uk>
2016-05-06 15:30:43 +0100

    further pester testing

--- test/pester/pester.tests.ps1
+++ test/pester/pester.tests.ps1
@@ -18,17 +18,57 @@ catch { exit 1 }
 
 Describe "SoftwareInstallations" {
     
-    Context "PowerShell" {
+    Context "Windows Management Framework" {
 
         It "PowerShell major version is greater than 3" {
 
-            $powershellversion = Invoke-Command -Session $session -ScriptBlock { $PSVersionTable.PSVersion.Major }
+            $powershellversion = Invoke-Command -Session $session -ScriptBlock { $PSVersionTable.PSVersion }
 
-            $powershellversion | should begreaterthan 4
+            $powershellversion.major | should begreaterthan 3
 
         }
 
     }
+    
+    Context "Puppet Agent" {
+
+        It "Puppet agent major version is 4" {
+
+            [version]$puppetagentversion = Invoke-Command -Session $session -ScriptBlock { puppet agent --version }
+
+            $puppetagentversion.major | should be 4
+
+        }
+        
+        It "puppetlabs-windows Puppet module is installed" {
+
+            $puppetmodulelist = Invoke-Command -Session $session -ScriptBlock { puppet module list }
+
+            $puppetmodulelist | should match 'puppetlabs-windows'
+
+        }
+        
+        It "Puppet agent service is running" {
+
+            $puppetagentservice = Invoke-Command -Session $session -ScriptBlock { get-service -name puppet }
+
+            $puppetagentservice.status | should be 'running'
+
+        }
+        
+    }
+
+    Context "Monitoring Agent" {
+        
+        It "NSClient++ agent service is running" {
+            
+            $nsclientagentservice = Invoke-Command -Session $session -ScriptBlock { get-service -name nscp }
+            
+            $nsclientagentservice.status | should be 'running'
+            
+        }
+        
+    }        
 
 }
 
